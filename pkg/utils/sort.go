9d158825d365a0ecf30d00cf83bf64164cd79102
tertawann <tertawan123@gmail.com>
2024-05-15 15:49:51 +0700

    Update unit test

--- pkg/utils/sort.go
+++ pkg/utils/sort.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gotamboon/modules/entities"
 )
 
-// current coding : find the best pratice to manage sort then write unit test.
 func SortDescDonatorsByTotal(rankingMaps map[string]*entities.DonatorRanking) []*entities.DonatorRanking {
 
 	donators := ConvertMapToSlice(rankingMaps, len(rankingMaps))
